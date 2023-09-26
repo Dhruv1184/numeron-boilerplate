ee8c20db354c924683d3c9186c103962351e5281
Dhruv1184 <dhruvkhadelwal1184@gmail.com>
2023-09-26 17:19:14 +0530

    Game completed

--- score.js
+++ score.js
@@ -1,1 +1,10 @@
 // Iteration 5: Store the player score and display it on the game over screen
+var playagain = document.getElementById('play-again-button');
+playagain.onclick = () => {
+    location.href = './game.html';
+}
+
+const scoreboard = document.getElementById('score-board');
+let score = localStorage.getItem('score');
+
+scoreboard.innerHTML = score;
